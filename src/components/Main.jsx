d1d6eb151c3ff08419f5a567c7e349e8a6a7defe
Mayank Chauhan <bobmayank1999@gmail.com>
2022-09-21 11:47:25 +0530

    firts ever commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,24 @@
+export default function Main() {
+    return (
+        <main className="main-content">
+            <h1 className="main-content--title">Interesting facts about React</h1>
+            <ul className="main-content--list">
+                <li className="main-content--list-item">
+                    Was first released in 2013
+                </li>
+                <li className="main-content--list-item">
+                    Was originally created by Jordan Walke
+                </li>
+                <li className="main-content--list-item">
+                    Has well over 100k stars on Github
+                </li>
+                <li className="main-content--list-item">
+                    Is maintained by Facebook
+                </li>
+                <li className="main-content--list-item">
+                    Poweres thousands of enterpise apps, including mobile apps
+                </li>
+            </ul>
+        </main>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-left">
+                <img className="navbar-left-logo" src="src\assets\reactjs-icon.png" />
+                <h1 className="navbar-left-title">ReactFacts</h1>
+            </div>
+            <p className="navbar-right">React Course - Project 1</p>
+        </nav>
+    )
+}
